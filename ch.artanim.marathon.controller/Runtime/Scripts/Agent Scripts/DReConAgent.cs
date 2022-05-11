4e6de6a77cb3e82460845c696c13e0f952c64f74
Joan Llobera <joan.llobera@artanim.ch>
2022-05-11 16:52:12 +0200

    reorganized the assets to avoid gaps in the structure of assemblies

--- ch.artanim.marathon.controller/Runtime/Scripts/Agent Scripts/DReConAgent.cs
+++ ch.artanim.marathon.controller/Runtime/Scripts/Agent Scripts/DReConAgent.cs
@@ -131,24 +131,5 @@ public interface IRememberPreviousActions
     public float[] PreviousActions { get; }
 }
 
-public interface IEventsAgent
-{
-    public event EventHandler<AgentEventArgs> onActionHandler;
-    public event EventHandler<AgentEventArgs> onBeginHandler;
-}
 
-public class AgentEventArgs: EventArgs
-{
-    public float[] actions;
-    public float reward;
-
-    public AgentEventArgs(float[] actions, float reward)
-    {
-        this.actions = actions;
-        this.reward = reward;
-    }
-
-    new public static AgentEventArgs Empty => new AgentEventArgs(new float[0], 0f);
-        
-}
 
